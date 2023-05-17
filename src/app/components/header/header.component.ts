d64fd011a1778d4e525d7a01e44fb3e977bbd1d0
maximebarber <maximebarber@gmail.com>
2023-05-17 14:52:14 +0200

    Added button to toggle form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   title: string = 'Task Tracker';
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   toggleAddTask() {
-    console.log('toggleAddTask')
+    this.uiService.toggleAddTask();
   }
 }
